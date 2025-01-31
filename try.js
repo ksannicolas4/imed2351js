8b20ace98549c26fbff5d7ecf81669eb7e3dba68
ksannicolas4 <107065312+ksannicolas4@users.noreply.github.com>
2025-01-31 05:34:46 -0500

    Update try.js

--- try.js
+++ try.js
@@ -1,11 +1,90 @@
+
 console.log('Testing') ;
 
-const name = "krj"
+const phan = "Flex";
+const globe = "Globe";
+const uppes= "Upper"
+
+
+const da= {
+    name: "Dauphin",
+    age: 32,
+    married: true,
+
+    sayName: function() {
+        console.log(this);
+    }
+ 
+};
+
+
+console.log(da.married)
+console.log(da.age)
+console.log(da.name)
+
+da.sayName();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function logger() {
-    const name = "krj"
-    console.log ("Party Time");
-    console.log ("Party Time");
-    console.log ("Party Time");
-    console.log ("Party Time");
+    const phan = "Flex"
+    console.log("Flex");
+    console.log("Flex");
 }
+
+console.log(phan);
+
+
+
+function logger() {
+    console.log(globe);
+    console.log("Globe");
+    console.log("Globe");
+}
+
+console.log(globe);
+
+
+function toUpper(text) {
+   const upperCased = text.toUpperCase();
+   console.log(upperCased);
+}
+
+toUpper(uppes);
+
+function adder(num1, num2) {
+    console.log(num1+num2);
+
+}
+
+adder(8,10);
+
+
+
+
+
+
+
+
