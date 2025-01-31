99172e4d8624c155096a90a1529070a34b74d2f4
ksannicolas4 <107065312+ksannicolas4@users.noreply.github.com>
2025-01-31 04:04:33 -0500

    n

--- try.js
+++ try.js
@@ -1,1 +1,11 @@
 console.log('Testing') ;
+
+const name = "krj"
+
+function logger() {
+    const name = "krj"
+    console.log ("Party Time");
+    console.log ("Party Time");
+    console.log ("Party Time");
+    console.log ("Party Time");
+}
